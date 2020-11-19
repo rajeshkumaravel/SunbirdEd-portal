aa932e93e1728fbb25b07c55227bd03d68d90ed8
Rajesh <rajesh.kumaravel@tarento.com>
2020-11-19 22:32:18 +0530

    Issue #SB-20650 fix: Updated logger and added blacklist module.

--- src/app/helpers/kongTokenHelper.js
+++ src/app/helpers/kongTokenHelper.js
@@ -5,14 +5,14 @@
  */
 // ******************************************************************************************************
 // ******************************************************************************************************
-// change device id to session id
-// meeting with Mathew
-// change workflow diagrams
-// code sanitization
 // check where all tokens used - generate report
 // What todo when kong device register fails to respond / service down
 //  - Shld we throw exception (or) just us fallback for using PORTAL_AUTH_TOKEN
-//  - Change in `getPortalAuthToken` method remove LHS
+//  - Change in `getPortalAuthToken` method remove LHS (or) use if else also log which token used
+
+// +++++++++++++++++++++++ TTL for non-logged in user +++++++++++++++++++++++
+// +++++++++++++++++++++++ Update TTL in server js and kongtokenhelper +++++++++++++++++++++++
+
 // ******************************************************************************************************
 // ******************************************************************************************************
 'use strict';
@@ -28,6 +28,7 @@ const KONG_DEVICE_REGISTER_TOKEN        = require('./environmentVariablesHelper.
 const KONG_DEVICE_REGISTER_AUTH_TOKEN   = require('./environmentVariablesHelper.js').KONG_DEVICE_REGISTER_AUTH_TOKEN;
 
 const KONG_SESSION_TOKEN                = 'kongDeviceToken';
+const BLACKLISTED_URL                   = ['/service/health', '/health'];
 
 const generateKongToken = async (req) => {
   return new Promise((resolve, reject) => {
@@ -48,13 +49,6 @@ const generateKongToken = async (req) => {
       sendRequest(options).then((response) => {
         const responseData = JSON.parse(response);
         if (_.get(responseData, 'params.status') === 'successful') {
-          // logger.info({
-          //   msg: 'KONG_TOKEN :: token generated success using key ' + _.get(req, 'sessionID') || 'no_key',
-          //   route: _.get(req, 'path'),
-          //   originalUrl: _.get(req, 'originalUrl') || 'no_originalUrl',
-          //   baseUrl: _.get(req, 'baseUrl') || 'no_originalUrl',
-          //   sessionId: _.get(req, 'sessionID')
-          // });
           _log(req, 'KONG_TOKEN :: token generated success using key ' + _.get(req, 'sessionID') || 'no_key');
           resolve(responseData);
         } else {
@@ -71,53 +65,45 @@ const generateKongToken = async (req) => {
 
 const registerDeviceWithKong = () => {
   return function (req, res, next) {
-    // logger.info({
-    //   msg: 'KONG_TOKEN :: requesting device register with kong',
-    //   route: _.get(req, 'path') || 'no_route',
-    //   originalUrl: _.get(req, 'originalUrl') || 'no_originalUrl',
-    //   baseUrl: _.get(req, 'baseUrl') || 'no_originalUrl',  
-    //   sessionId: _.get(req, 'sessionID')
-    // });
     _log(req, 'KONG_TOKEN :: requesting device register with kong');
-    if (KONG_DEVICE_REGISTER_TOKEN !== 'false' && !getKongTokenFromSession(req)) {
-      generateKongToken(req).then((kongToken) => {
-        if (_.get(kongToken, 'result.token')) {
-          req.session[KONG_SESSION_TOKEN] = _.get(kongToken, 'result.token');
-          req.session.save((err) => {
-            if (err) {
-              next(err);
-            } else {
-              next();
-            }
-          });
-        } else {
-          logger.error({
-            'id': 'api.kong.tokenManager', 'ts': new Date(),
-            'params': {
-              'resmsgid': uuidv1(),
-              'msgid': uuidv1(),
-              'err': 'Internal Server Error',
-              'status': 'Internal Server Error',
-              'errmsg': 'Internal Server Error'
-            },
-            'responseCode': 'Internal Server Error',
-            'result': {}
-          });
-          next(new Error('api.kong.tokenManager:: Internal Server Error'));
-        }
-      }).catch((err) => {
-        next(err);
-      });
+    // Check if URL is blacklisted; forward request in case blacklisted
+    if (!unless(req)) {
+      if (KONG_DEVICE_REGISTER_TOKEN !== 'false' && !getKongTokenFromSession(req)) {
+        generateKongToken(req).then((kongToken) => {
+          if (_.get(kongToken, 'result.token')) {
+            req.session[KONG_SESSION_TOKEN] = _.get(kongToken, 'result.token');
+            req.session.save((err) => {
+              if (err) {
+                next(err);
+              } else {
+                next();
+              }
+            });
+          } else {
+            logger.error({
+              'id': 'api.kong.tokenManager', 'ts': new Date(),
+              'params': {
+                'resmsgid': uuidv1(),
+                'msgid': uuidv1(),
+                'err': 'Internal Server Error',
+                'status': 'Internal Server Error',
+                'errmsg': 'Internal Server Error'
+              },
+              'responseCode': 'Internal Server Error',
+              'result': {}
+            });
+            next(new Error('api.kong.tokenManager:: Internal Server Error'));
+          }
+        }).catch((err) => {
+          next(err);
+        });
+      } else {
+        let _msg = getKongTokenFromSession(req) ? 'using existing token' : 'no token from session'
+        _log(req, 'KONG_TOKEN :: request denied - either flag is set to false or ' + _msg);
+        next();
+      }
     } else {
-      let _msg = getKongTokenFromSession(req) ? 'using existing token' : 'no token from session'
-      // logger.info({
-      //   msg: 'KONG_TOKEN :: request denied - either flag is set to false or ' + _msg,
-      //   route: _.get(req, 'path') || 'no_route',
-      //   originalUrl: _.get(req, 'originalUrl') || 'no_originalUrl',
-      //   baseUrl: _.get(req, 'baseUrl') || 'no_originalUrl',  
-      //   sessionId: _.get(req, 'sessionID')
-      // });
-      _log(req, 'KONG_TOKEN :: request denied - either flag is set to false or ' + _msg);
+      _log(req, 'KONG_TOKEN :: request denied - URL blacklisted');
       next();
     }
   }
@@ -127,14 +113,12 @@ const getKongTokenFromSession = (req) => {
   return _.get(req, 'session.' + KONG_SESSION_TOKEN);
 };
 
+const unless = (req) => {
+  const existsIndex = _.indexOf(BLACKLISTED_URL, _.get(req, 'originalUrl'));
+  return existsIndex > -1 ? true : false;
+};
+
 const getPortalAuthToken = (req) => {
-  // logger.info({
-  //   msg: (KONG_DEVICE_REGISTER_TOKEN === 'true') ? 'USE_KONG_TOKEN' : 'USE_PORTAL_TOKEN',
-  //   route: _.get(req, 'path') || 'no_route',
-  //   originalUrl: _.get(req, 'originalUrl') || 'no_originalUrl',
-  //   baseUrl: _.get(req, 'baseUrl') || 'no_originalUrl',
-  //   token: _.get(req, 'session.' + KONG_SESSION_TOKEN)
-  // });
   _log(req, (KONG_DEVICE_REGISTER_TOKEN === 'true') ? 'USE_KONG_TOKEN' : 'USE_PORTAL_TOKEN');
   return (KONG_DEVICE_REGISTER_TOKEN === 'true') ? _.get(req, 'session.' + KONG_SESSION_TOKEN) : PORTAL_API_AUTH_TOKEN;
 };
@@ -144,7 +128,7 @@ const _log = (req, message) => {
     msg: message,
     route: _.get(req, 'path') || 'no_route',
     originalUrl: _.get(req, 'originalUrl') || 'no_originalUrl',
-    baseUrl: _.get(req, 'baseUrl') || 'no_originalUrl',
+    baseUrl: _.get(req, 'baseUrl') || 'baseUrl',
     token: _.get(req, 'session.' + KONG_SESSION_TOKEN)
   });
 };
